d6bd8110fd4f6b71106b4155fa35b3fb13783334
Parth Choudhary <parth.dudhwal@gmail.com>
2024-02-13 21:59:48 +0530

    Setting up routes for login and registration

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 require('dotenv').config();
 const dbConfig = require('./config/dbConfig');
 const port = process.env.Port || 5000;
+
+const usersRoute = require('./routes/userRoute');
+app.use('/api/user', usersRoute);
 
 app.listen(port, () => {
     console.log(`Node server started on port ${port}`);
