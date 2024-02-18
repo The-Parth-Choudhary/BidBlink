ab259b22c47394cfc03e3e95d180c580f01cda02
Parth Choudhary <parth.dudhwal@gmail.com>
2024-02-18 14:40:14 +0530

    fixed login glitch

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -20,7 +20,7 @@ function Login() {
             if (response.success) {
                 message.success(response.message);
                 localStorage.setItem('token', response.data);
-                navigate('/');
+                window.location.href = '/'
             }
             else {
                 message.error(response.message);
