a9ae700d5957ba3575a15d7437699a40ea685c7a
Mukeshkumar1611 <mukesh0895.be21@chitkara.edu.in>
2024-02-27 11:56:57 +0530

    added edit product and delete product functionality

--- client/src/pages/Profile/Products/ProductForm.js
+++ client/src/pages/Profile/Products/ProductForm.js
@@ -1,10 +1,9 @@
 import { Col, Form, Input, Modal, Row, Tabs, message } from 'antd';
 import TextArea from 'antd/es/input/TextArea';
 import { useDispatch, useSelector } from 'react-redux';
-import React from 'react';
-import { AddProduct } from '../../../apicalls/products';
+import React, { useEffect } from 'react';
+import { AddProduct, EditProduct } from '../../../apicalls/products';
 import { SetLoader } from '../../../redux/loadersSlice'
-
 
 const additionalThings = [
     {
@@ -32,19 +31,34 @@ const rules = [
     }
 ];
 
-function ProductForm({ showProductForm, setShowProductForm }) {
+function ProductForm({ showProductForm, setShowProductForm, selectedProduct, getData }) {
     const dispatch = useDispatch();
     const { user } = useSelector(state => state.users);
     const formRef = React.useRef(null);
 
+
+    useEffect(() => {
+        if (selectedProduct) {
+            formRef.current.setFieldsValue(selectedProduct)
+        }
+
+    }, [selectedProduct])
+
     const onFinish = async (values) => {
         try {
-            values.seller = user;
             dispatch(SetLoader(true));
-            const response = await AddProduct(values);
+            let response = null;
+            if (selectedProduct) {
+                response = await EditProduct(selectedProduct._id, values);
+            }
+            else {
+                values.seller = user;
+                response = await AddProduct(values);
+            }
             dispatch(SetLoader(false));
             if (response.success) {
                 message.success(response.message);
+                getData();
                 setShowProductForm(false);
             }
             else {
@@ -57,61 +71,67 @@ function ProductForm({ showProductForm, setShowProductForm }) {
     };
 
     return (
-        <Modal title='' open={showProductForm} onCancel={() => setShowProductForm(false)} centered width={1000} okText='Save' onOk={() => {
+        <Modal title='' open={showProductForm} onCancel={() => {
+            setShowProductForm(false);
+
+        }} centered width={1000} okText='Save' onOk={() => {
             formRef.current.submit();
         }}>
-            <Tabs defaultActiveKey='1'>
-                <Tabs.TabPane tab='General' key='1'>
-                    <Form layout='vertical' ref={formRef} onFinish={onFinish}>
-                        <Form.Item label='Name' name='name' rules={rules}>
-                            <Input type='text' />
-                        </Form.Item>
-                        <Form.Item label='Description' name='description' rules={rules}>
-                            <TextArea type='text' />
-                        </Form.Item>
+            <div>
+                <h1 className="text-primary text-2xl text-center font-semibold uppercase">{selectedProduct ? 'Edit Product' : 'Add Prodcut'}</h1>
+                <Tabs defaultActiveKey='1'>
+                    <Tabs.TabPane tab='General' key='1'>
+                        <Form layout='vertical' ref={formRef} onFinish={onFinish}>
+                            <Form.Item label='Name' name='name' rules={rules}>
+                                <Input type='text' />
+                            </Form.Item>
+                            <Form.Item label='Description' name='description' rules={rules}>
+                                <TextArea type='text' />
+                            </Form.Item>
 
-                        <Row gutter={[16, 16]}>
-                            <Col span={8}>
-                                <Form.Item label='Price' name='price' rules={rules}>
-                                    <Input type='number' />
-                                </Form.Item>
-                            </Col>
-                            <Col span={8}>
-                                <Form.Item label='Category' name='category' rules={rules}>
-                                    <select>
-                                        <option value=''>Select</option>
-                                        <option value='electronics'>Electronics</option>
-                                        <option value='fashion'>Fashion</option>
-                                        <option value='home'>Home</option>
-                                        <option value='sports'>Sports</option>
-                                    </select>
-                                </Form.Item>
-                            </Col>
+                            <Row gutter={[16, 16]}>
+                                <Col span={8}>
+                                    <Form.Item label='Price' name='price' rules={rules}>
+                                        <Input type='number' />
+                                    </Form.Item>
+                                </Col>
+                                <Col span={8}>
+                                    <Form.Item label='Category' name='category' rules={rules}>
+                                        <select>
+                                            <option value=''>Select</option>
+                                            <option value='electronics'>Electronics</option>
+                                            <option value='fashion'>Fashion</option>
+                                            <option value='home'>Home</option>
+                                            <option value='sports'>Sports</option>
+                                        </select>
+                                    </Form.Item>
+                                </Col>
 
-                            <Col span={8}>
-                                <Form.Item label='Age' name='age' rules={rules}>
-                                    <Input type='number' />
-                                </Form.Item>
-                            </Col>
-                        </Row>
+                                <Col span={8}>
+                                    <Form.Item label='Age' name='age' rules={rules}>
+                                        <Input type='number' />
+                                    </Form.Item>
+                                </Col>
+                            </Row>
 
-                        <div className='flex gap-10'>
-                            {additionalThings.map((item, index) => (
-                                <Form.Item key={index} label={item.label} name={item.name}>
-                                    <Input type='checkbox' onChange={(e) => {
-                                        formRef.current.setFieldsValue({
-                                            [item.name]: e.target.checked
-                                        })
-                                    }}
-                                        checked={formRef.current?.getFieldValue(item.name)}
-                                    />
-                                </Form.Item>
-                            ))}
-                        </div>
-                    </Form>
-                </Tabs.TabPane>
-                <Tabs.TabPane tab='Images' key='2'><h1>Images</h1></Tabs.TabPane>
-            </Tabs>
+                            <div className='flex gap-10'>
+                                {additionalThings.map((item, index) => (
+                                    <Form.Item key={index} label={item.label} name={item.name} valuePropName='checked'>
+                                        <Input type='checkbox' onChange={(e) => {
+                                            formRef.current.setFieldsValue({
+                                                [item.name]: e.target.checked
+                                            })
+                                        }}
+                                            checked={formRef.current?.getFieldValue(item.name)}
+                                        />
+                                    </Form.Item>
+                                ))}
+                            </div>
+                        </Form>
+                    </Tabs.TabPane>
+                    <Tabs.TabPane tab='Images' key='2'><h1>Images</h1></Tabs.TabPane>
+                </Tabs>
+            </div>
         </Modal>
     );
 }
--- server/routes/productsRoute.js
+++ server/routes/productsRoute.js
@@ -22,10 +22,42 @@ router.post('/add-product', authMiddleware, async (req, res) => {
 // get all products
 router.get('/get-products', async (req, res) => {
     try {
-        const products = await Product.find();
+        const products = await Product.find().sort({ createdAt: -1 });
         res.send({
             success: true,
             products
+        })
+    } catch (error) {
+        res.send({
+            success: false,
+            message: error.message
+        })
+    }
+})
+
+// edit product
+router.put('/edit-product/:id', authMiddleware, async (req, res) => {
+    try {
+        await Product.findByIdAndUpdate(req.params.id, req.body);
+        res.send({
+            success: true,
+            message: 'Product updated successfully'
+        })
+    } catch (error) {
+        res.send({
+            success: false,
+            message: error.message
+        })
+    }
+})
+
+// delete product
+router.delete('/delete-product/:id', authMiddleware, async (req, res) => {
+    try {
+        await Product.findByIdAndDelete(req.params.id);
+        res.send({
+            success: true,
+            message: 'Product deleted successfully'
         })
     } catch (error) {
         res.send({
