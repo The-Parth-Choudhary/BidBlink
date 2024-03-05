86c3ac1a23e6bf5524870119e92eee8e56fb9a3a
Parth Choudhary <parth.dudhwal@gmail.com>
2024-03-05 18:24:01 +0530

    Trying to deploy on render

--- server/server.js
+++ server/server.js
@@ -15,6 +15,16 @@ app.use('/api/products', productsRoute);
 app.use('/api/bids', bidsRoute);
 app.use('/api/notifications', notificationsRoute);
 
+// deployment config
+const path = require('path');
+__dirname = path.resolve();
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/client/build')));
+    app.get('*', (req, res)=>{
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 app.listen(port, () => {
     console.log(`Node server started on port ${port}`);
 })
