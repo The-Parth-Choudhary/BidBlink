dde2e5201514c6a4d4e81f562ad6173b25cef44e
Parth Choudhary <parth.dudhwal@gmail.com>
2024-03-09 19:02:01 +0530

    Fixed loader bug

--- client/src/redux/loadersSlice.js
+++ client/src/redux/loadersSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 export const loadersSlice = createSlice({
     name: 'loaders',
     initialState: {
-        loading: true
+        loading: false
     },
     reducers: {
         SetLoader: (state, action) => {
